672f13566e45b0029a3e7b28f0ac27ba664b6009
Stanley Metray <stanleymetray@gmail.com>
2022-10-25 14:05:22 +0530

    cors access enabled to multiple urls

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,9 @@ const PORT = process.env.PORT || 5000;
 const MONGO_URI = "mongodb+srv://stanley-metray:0OIzQx8Eu3sj1kOb@task-manager.rxta0cd.mongodb.net/Task_Manager?retryWrites=true&w=majority";
 //Setting up all middlewares
 
-app.use(cors());
+app.use(cors({
+    origin : ["http://localhost:3000","https://mern-task-manager-app.onrender.com"]
+}));
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.use(TaskManagerRouter);
